d70766333fff0b551697a34134011ec562d6dfc3
mexuscloud <iammexuscloud@gmail.com>
2023-10-19 03:45:31 +0000

    #27 Enabled static website hosting, uploaded index and error file via terraform

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -18,3 +18,40 @@ resource "aws_s3_bucket" "website_bucket" {
     UserUuid   = var.user_uuid
   }
 }
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = var.index_html_filepath
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  etag = filemd5(var.index_html_filepath)
+  #
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = var.error_html_filepath
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  etag = filemd5(var.error_html_filepath)
+  #
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket name for our static web hosting"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+  description = "s3 static website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
