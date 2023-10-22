5fdb6aea3adbd0256b25ad549bd94ae019e52e13
mexuscloud <iammexuscloud@gmail.com>
2023-10-22 04:35:54 +0000

    #29 create cdn and service s3 bucket from cdn

--- Journal/week1.md
+++ Journal/week1.md
@@ -202,3 +202,45 @@ resource "aws_s3_object" "index_html" {
   source = "${path.root}/public/index.html"
 }
 ```
+
+## Terraform Locals 
+
+Locals allows us to define local variables. 
+It can be very useful when we need terraform data into another formate and have it referenced as a variable. 
+
+```tf
+locals {
+  s3_origin_id = "MyS3Origin"
+}
+```
+[Local Values](https://developer.hashicorp.com/terraform/language/values/locals)
+
+## Terraform Data Sources 
+
+This allows us to source data from cloud resources. 
+
+This is useful when we want to reference cloud resources without imorting them. 
+
+eg. 
+
+```tf
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+```
+
+[Data Sources](https://developer.hashicorp.com/terraform/language/data-sources)
+
+## Working with JSON 
+
+we use the jsonencode to create the json policy inline in the hcl
+
+```tf
+> jsonencode({"hello"="world"})
+{"hello":"world"}
+```
+
+[jsonencode)](https://developer.hashicorp.com/terraform/language/functions/jsonencode)
+
--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -7,51 +7,8 @@ terraform {
   }
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
 
-resource "aws_s3_bucket" "website_bucket" {
-  # AWS bucket naming rules 
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = var.bucket_name
+data "aws_caller_identity" "current" {}
 
-  tags = {
-    UserUuid   = var.user_uuid
-  }
-}
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
 
-resource "aws_s3_bucket_website_configuration" "website_configuration" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = var.index_html_filepath
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  etag = filemd5(var.index_html_filepath)
-  #
-}
-
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  source = var.error_html_filepath
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  etag = filemd5(var.error_html_filepath)
-  #
-}
