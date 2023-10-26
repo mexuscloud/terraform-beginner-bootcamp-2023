f3af335c98e6bf767fd9a40a847282b2bc7e0d91
mexuscloud <iammexuscloud@gmail.com>
2023-10-26 00:35:49 +0000

    try and fix issues with terraform variables in terraform cloud

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "website_bucket" {
 
   tags = {
     UserUuid   = var.user_uuid
-    Hello = "world"
+    Hello = "Mars"
   }
 }
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
@@ -29,24 +29,24 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
-  source = var.index_html_filepath
+  source = "${path.root}${var.index_html_filepath}"
   content_type = "text/html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  etag = filemd5(var.index_html_filepath)
-  #
+  etag = filemd5("${path.root}${var.index_html_filepath}")
+  
 }
 
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
-  source = var.error_html_filepath
+  source = "${path.root}${var.error_html_filepath}"
   content_type = "text/html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  etag = filemd5(var.error_html_filepath)
+  etag = filemd5("${path.root}${var.error_html_filepath}")
   #
 }
 
