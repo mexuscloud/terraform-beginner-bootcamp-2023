dc4c4362b9eea7b0461d522d8a34bd7dc7e798ee
mexuscloud <iammexuscloud@gmail.com>
2023-10-26 17:04:29 +0000

    #40 made sure all of our endpoints worked correctly

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -199,7 +199,6 @@ class TerraTownsMockServer < Sinatra::Base
     # Validate payload data
     name = payload["name"]
     description = payload["description"]
-    domain_name = payload["domain_name"]
     content_version = payload["content_version"]
 
     unless params[:uuid] == $home[:uuid]
@@ -208,9 +207,9 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
+    home.domain_name = $home[:domain_name]
     home.name = name
     home.description = description
-    home.domain_name = domain_name
     home.content_version = content_version
 
     unless home.valid?
@@ -231,9 +230,10 @@ class TerraTownsMockServer < Sinatra::Base
       error 404, "failed to find home with provided uuid and bearer token"
     end
 
-    # delete from our mock database 
+    # delete from our mock database
+    uuid = $home[:uuid] 
     $home = {}
-    { message: "House deleted successfully" }.to_json
+    { uuid: uuid }.to_json
   end
 end
 
