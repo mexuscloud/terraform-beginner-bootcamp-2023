40d29505db44d34e4cdc14b6b33e2701e66a7279
mexuscloud <iammexuscloud@gmail.com>
2023-10-26 01:11:19 +0000

    un-commenting code

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -26,29 +26,29 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 
-# resource "aws_s3_object" "index_html" {
-#   bucket = aws_s3_bucket.website_bucket.bucket
-#   key    = "index.html"
-#   source = "${path.root}${var.index_html_filepath}"
-#   content_type = "text/html"
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = "${path.root}${var.index_html_filepath}"
+  content_type = "text/html"
 
-#   # The filemd5() function is available in Terraform 0.11.12 and later
-#   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-#   etag = filemd5("${path.root}${var.index_html_filepath}")
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  etag = filemd5("${path.root}${var.index_html_filepath}")
   
-# }
+}
 
-# resource "aws_s3_object" "error_html" {
-#   bucket = aws_s3_bucket.website_bucket.bucket
-#   key    = "error.html"
-#   source = "${path.root}${var.error_html_filepath}"
-#   content_type = "text/html"
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = "${path.root}${var.error_html_filepath}"
+  content_type = "text/html"
 
-#   # The filemd5() function is available in Terraform 0.11.12 and later
-#   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-#   etag = filemd5("${path.root}${var.error_html_filepath}")
-#   #
-# }
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  etag = filemd5("${path.root}${var.error_html_filepath}")
+  #
+}
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy
 
