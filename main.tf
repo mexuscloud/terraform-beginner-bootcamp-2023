bc95f501854e69515d34d5221b4e1668ce970ac2
mexuscloud <iammexuscloud@gmail.com>
2023-11-12 06:39:55 +0000

    #47 test our terrahome on terratowns test town

--- main.tf
+++ main.tf
@@ -14,20 +14,20 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   assets_path = var.assets_path
-#   content_version = var.content_version
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
+  content_version = var.content_version
+}
 
 resource "terratowns_home" "home" {
   name = "How to be an expert in playing FIFA!"
@@ -35,9 +35,8 @@ resource "terratowns_home" "home" {
   FIFA is one of the most popular sport games in the world
 
 DESCRIPTION
-  # domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "abcd34.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
   content_version = 1
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-# output "bucket_name" {
-#   description = "Bucket name for our static web hosting"
-#   value = module.terrahouse_aws.bucket_name
-# }
+output "bucket_name" {
+  description = "Bucket name for our static web hosting"
+  value = module.terrahouse_aws.bucket_name
+}
 
-# output "s3_website_endpoint" {
-#   description = "s3 static website hosting endpoint"
-#   value = module.terrahouse_aws.website_endpoint
-# }
+output "s3_website_endpoint" {
+  description = "s3 static website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
 
-# output "cloudfront_url" {
-#   description = "The CloudFront Distribution Domain Name"
-#   value = module.terrahouse_aws.cloudfront_url
-# }
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
+}
